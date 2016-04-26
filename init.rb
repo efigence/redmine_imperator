baf2c02749b30cee1febc48253138c44e711ff8e
Marcin Kalita <rubyconvict@gmail.com>
2016-04-26 10:00:51 +0200

    generate plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_imperator do
+  name 'Redmine Imperator plugin'
+  author 'Marcin Kalita'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/efigence/redmine_imperator'
+  author_url 'http://www.efigence.com/'
+end
