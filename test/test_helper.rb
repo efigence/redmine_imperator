db2804332d284d68cd31b54d7551b73b0bb9526b
Marcin Kalita <rubyconvict@gmail.com>
2016-04-26 15:02:14 +0200

    test the truth

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,19 @@
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'codeclimate-test-reporter'
+require 'simplecov'
+require 'coveralls'
+
+# Configure Rails Envinronment
+ENV['RAILS_ENV'] = 'test'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    CodeClimate::TestReporter::Formatter,
+    Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start
+require 'minitest/autorun'
---
+++ test/truth_test.rb
@@ -0,0 +1,11 @@
+require File.dirname(__FILE__) + '/test_helper'
+
+class Truth
+end
+
+class TruthTest < ActiveSupport::TestCase
+
+  def test_the_truth
+    assert true
+  end
+end
