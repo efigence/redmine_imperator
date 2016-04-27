a454b07c30b71454002853f690c272b7016964ab
Marcin Kalita <rubyconvict@gmail.com>
2016-04-27 15:14:21 +0200

    copy controller filters and try real tests

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,9 +1,20 @@
 class ApiController < UsersController # < ActionController::Base
   unloadable
-  accept_api_auth :index, :create
-  before_filter :set_current_user
-  skip_before_filter :check_if_login_required
+  #before_filter :set_current_user
+
+  # appliction_controller
   skip_before_filter :verify_authenticity_token
+  skip_before_filter :session_expiration, :user_setup, :check_if_login_required,
+                     :check_password_change, :set_localization
+
+  # users_controller
+  skip_before_filter :require_admin
+  accept_api_auth :index, :show, :create, :update, :destroy
+  before_filter :find_user, :only => [:show, :edit, :update, :destroy]
+  accept_api_auth :index, :show, :create, :update, :destroy
+
+  # custom
+  before_filter :disable_sudo_mode
 
   def index
     super
@@ -11,5 +22,9 @@ class ApiController < UsersController # < ActionController::Base
 
   def create
     super
+  end
+
+  def disable_sudo_mode
+    SudoMode.disable!
   end
 end
--- test/integration/api_test.rb
+++ test/integration/api_test.rb
@@ -5,22 +5,22 @@ class ApiTest < Redmine::ApiTest::Base
 
   test 'GET /api.json should return users' do
     assert true
-    #get '/api.json', {}, credentials('admin')
-    #assert_response :success
-    #assert_equal 'application/json', response.content_type
-    #assert_select 'users', assigns(:users).size
+    get '/api.json', {}, credentials('admin')
+    assert_response :success
+    assert_equal 'application/json', response.content_type
+    assert_select 'users', assigns(:users).size
   end
 
   def test_create
     assert true
-    #assert_difference('User.count') do
-    #  post '/api.json', {
-    #    user: {
-    #      login: 'foo', firstname: 'Firstname', lastname: 'Lastname',
-    #      mail: 'foo@example.net', password: 'secret123'
-    #    }
-    #  }, {}
-    #  assert_response 401
-    #end
+    assert_difference('User.count') do
+      post '/api.json', {
+        user: {
+          login: 'foo', firstname: 'Firstname', lastname: 'Lastname',
+          mail: 'foo@example.net', password: 'secret123'
+        }
+      }, {}
+      assert_response 401
+    end
   end
 end
