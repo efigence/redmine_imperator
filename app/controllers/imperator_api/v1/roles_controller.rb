67f35c554b0d35da8e889410b9aa43dfe5f2e4e7
Marek Lipka <mlipka@artegence.com>
2016-05-19 13:01:23 +0200

    roles management unlocked through API

--- app/controllers/imperator_api/v1/roles_controller.rb
+++ app/controllers/imperator_api/v1/roles_controller.rb
@@ -1,6 +1,7 @@
 module ImperatorApi
   module V1
     class RolesController < ::RolesController
+      accept_api_auth :index, :show, :create, :update, :destroy
       include Concerns::ErrorHandling
       include Concerns::AccessControl
     end
